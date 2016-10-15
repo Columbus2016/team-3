0cea8ed24bd4a631a4cf3d57b578d01c6254f324
Seth Delbridge <sadelbrid@gmail.com>
2016-10-14 20:35:03 -0400

    #ImWithCode login setup

---
+++ SGKApp/client/templates/login/login.js
@@ -0,0 +1,19 @@
+import './login.html';
+import { Template } from 'meteor/templating';
+import 'login.html';
+
+var signingUp = true;
+
+var button = document.getElementById('button');
+console.log(button);
+button.innerHTML = "test"
+button.onclick = function(){
+    signingUp = !signingUp;
+
+    if(signingUp){
+        button.innerText = "Sign up";
+    }
+    else{
+        button.innerText = "Log in";
+    }
+};

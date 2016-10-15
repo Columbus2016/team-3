39c188ac0726b459c0b493c7304c4301a85d325f
Cyriac Domini <cyriacd@outlook.com>
2016-10-15 03:02:53 -0400

    #MakeCodeGreatAgain fix devise commits

--- SGK/app/models/user.rb
+++ SGK/app/models/user.rb
@@ -3,11 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  has_many :chat_rooms
-  has_many :messages, dependent: :destroy
-
-  def name
-    email.split('@')[0]
-  end
 end
